a2bdc9c407265e69c0b83b90b192f4d54344c143
TheDarkStrix <thedarkstrix@gmail.com>
2020-11-21 14:15:58 +0530

    Add click functionality
    ** Check for game over or win condition
    ** Add functionality to dig and flag mines
    ** Display total available mine count
    ** Add the spread out functionality
    ** Render out images when items are placed {bomb and flag}

--- src/components/cell.js
+++ src/components/cell.js
@@ -1,12 +1,44 @@
 import React from "react"
 
+import Bomb from "./images/bomb.png"
+import Flag from "./images/flag.png"
 export default class Cell extends React.Component {
-  getValue() {}
+  getCellValue() {
+    if (!this.props.value.isRevealed) {
+      return this.props.value.isFlagged ? (
+        <span class="sweeper-icons">
+          <img src={Flag} />
+        </span>
+      ) : null
+    }
+    if (this.props.value.isMine) {
+      return (
+        <span class="sweeper-icons">
+          <img src={Bomb} />
+        </span>
+      )
+    }
+    if (this.props.value.neighbour === 0) {
+      return null
+    }
+    return this.props.value.neighbour
+  }
 
   render() {
+    let className =
+      "cell" +
+      (this.props.value.isRevealed ? "" : " hidden") +
+      (this.props.value.isMine ? " is-mine" : "") +
+      (this.props.value.isFlagged ? " is-flag" : "")
+
     return (
-      <div ref="cell" className="cell hidden">
-        {this.getValue()}
+      <div
+        ref="cell"
+        onClick={this.props.onClick}
+        className={className}
+        onContextMenu={this.props.cMenu}
+      >
+        {this.getCellValue()}
       </div>
     )
   }
