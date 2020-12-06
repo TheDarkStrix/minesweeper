270e18460cfef95d1266208e762c347a6d792169
TheDarkStrix <thedarkstrix@gmail.com>
2020-12-06 18:53:24 +0530

    Add service worker

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,21 @@
 
 module.exports = {
   /* Your site config here */
-  plugins: [],
+  plugins: [
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Minesweeper`,
+        short_name: `Minesweeper`,
+        start_url: `/`,
+        background_color: `#f7f0eb`,
+        theme_color: `#a2466c`,
+        display: `standalone`,
+        icon: `src/includes/img/mine.svg`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-offline`,
+    },
+  ],
 }
